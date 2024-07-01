732a7b69e1543a034bd720878d422d70f8859c2c
brucekid96 <kazebrice98@gmail.com>
2024-07-01 14:10:03 +0200

    Updated product list and recent orders tables, implemented state management using context for adding new products.

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,54 +1,65 @@
-import React from 'react'
+import React,{useContext,useState} from 'react'
+import './Home.css'
+import DataTable from 'react-data-table-component';
+import { ProductContext } from '../contexts/ProductContext';
 import {BsFillArchiveFill,BsFillGrid3X3GapFill,
   BsPeopleFill,BsFillBellFill} from 'react-icons/bs'
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,LineChart, Line } from 'recharts';
+
 
 
 function Home() {
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
+  const { products } = useContext(ProductContext);
+
+  const columns = [
+  
+   {
+    name: 'Product Name',
+    selector: row => row.name,
+    sortable:true
+   },
+   {
+    name: 'Size',
+    selector: row => row.size,
+    sortable:true
+   } ,
+   {
+    name: 'Price',
+    selector: row => row.newPrice,
+    sortable:true
+   } ,
+   {
+    name: 'Quantity',
+    selector: row => numberOfUniqueShoes,
+    sortable:true
+   } ,
+   {
+    name: 'Amount',
+    selector: row => row.newPrice* numberOfUniqueShoes,
+    sortable:true
+   } ,
   ];
+  const data = products;
+  const [records, setRecords] = useState(data);
+  function handleFilter(event) {
+    const newData = data.filter(row => {
+      return row.product_name.toLowerCase().includes(event.target.value.toLowerCase())
+    })
+    setRecords(newData)
+  }
+
+      // Create a Set to store unique shoe names
+      const uniqueShoeNames = new Set();
+
+      // Extract and add shoe names to the Set
+      products.forEach((product) => {
+        const cleanedName = product.name.trim(); // Supprime les espaces vides au début et à la fin
+        if (cleanedName) {
+          uniqueShoeNames.add(cleanedName);
+        }
+      });
+    
+      // Get the count of unique shoe names
+      const numberOfUniqueShoes = uniqueShoeNames.size;
   return (
     <main className="main-container">
     <div className='main-title'>
@@ -65,71 +76,36 @@ function Home() {
     </div>
     <div className="card">
       <div className="card-inner">
-        <h3>Categories</h3>
+        <h3>New subscriptions</h3>
         <BsFillGrid3X3GapFill className='card-iicon'/>
       </div>
       <h1>12</h1>
     </div>
     <div className="card">
       <div className="card-inner">
-        <h3>CUSTOMERS</h3>
+        <h3>New orders</h3>
         < BsPeopleFill className='card-iicon'/>
       </div>
       <h1>33</h1>
     </div>
     <div className="card">
       <div className="card-inner">
-        <h3>ALERTS</h3>
+        <h3>Avg. order revenue</h3>
         <BsFillBellFill className='card-iicon'/>
       </div>
-      <h1>42</h1>
+      <h1>$1,080</h1>
     </div>
   </div>
-   
-  <div className="charts">
-  <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-         <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-          <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
-        </BarChart>
-      </ResponsiveContainer>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
-  </div>
+  <div className="cartitems">
+    
+  <DataTable
+  columns={columns}
+  data={records}
+  fixedHeader
+  pagination
+  ></DataTable>
+</div>
+
 
     </main>
   )
--- src/Components/Products/ProductListForm.jsx
+++ src/Components/Products/ProductListForm.jsx
@@ -1,10 +1,58 @@
 import React, { useContext,useState } from 'react'
 import './ProductListForm.css'
+import DataTable from 'react-data-table-component'
 import { ProductContext } from '../../contexts/ProductContext'
 import action_icon from '../Assets/action.png'
+
 const ProductListForm = () => {
   const { products } = useContext(ProductContext);
-
+  const columns = [
+    {
+      name: 'Product',
+      selector: row =>row.image && (
+        <img
+          src={URL.createObjectURL(row.image)} // Use the actual image URL or base64 data here
+          
+          className='carticon-product-icon'
+        />
+      )
+      ,
+      sortable:true
+     },
+    {
+     name: 'Product Name',
+     selector: row => row.name,
+     sortable:true
+    },
+    {
+     name: 'Size',
+     selector: row => row.size,
+     sortable:true
+    } ,
+    {
+     name: 'Price',
+     selector: row => row.newPrice,
+     sortable:true
+    } ,
+    {
+     name: 'Quantity',
+     selector: row => numberOfUniqueShoes,
+     sortable:true
+    } ,
+    {
+     name: 'Amount',
+     selector: row => row.newPrice* numberOfUniqueShoes,
+     sortable:true
+    } ,
+   ];
+   const data = products;
+   const [records, setRecords] = useState(data);
+   function handleFilter(event) {
+    const newData = data.filter(row => {
+      return row.product_name.toLowerCase().includes(event.target.value.toLowerCase())
+    })
+    setRecords(newData)
+  }
     // Create a Set to store unique shoe names
     const uniqueShoeNames = new Set();
 
@@ -21,17 +69,23 @@ const ProductListForm = () => {
   return (
     <div className='cartitems'>
 
-      <ul className="cartitems-format-main">
+      {/* <ul className="cartitems-format-main">
         <li>Products</li>
-        <li>Title</li>
-        <li>Model</li>
+        <li>Name</li>
         <li>Size</li>
         <li>Quantity</li>
         <li>Price</li>
         <li>Action</li>
       </ul>
-      <hr />
-      
+      <hr /> */}
+
+<DataTable
+  columns={columns}
+  data={records}
+  fixedHeader
+  pagination
+  ></DataTable>
+{/*       
           <div>
           {products.map((product) => (
          <div key={product.id} className="cartitems-format cartitems-format-main">
@@ -43,8 +97,7 @@ const ProductListForm = () => {
               />
             )}
              <p>{product.name}</p>
-             <p>{product.model}</p>
-             <p>{product.size}</p>
+              <p>{product.size}</p>
              <button className="cartitems-quantity">{numberOfUniqueShoes}</button>
              <p>${product.newPrice}</p>
              <img src={action_icon}  alt="" className='cartitems-action-icon'/>
@@ -52,7 +105,7 @@ const ProductListForm = () => {
           ))}
          <hr />
          
-       </div>
+       </div> */}
        
         
 
